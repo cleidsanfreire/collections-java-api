061010919229ed57de694e33bd8a4439facfbc9a
Freire Dev <dsanmike12@gmail.com>
2024-05-29 10:38:54 -0300

    Primeiro commit

---
+++ src/Box.java
@@ -0,0 +1,10 @@
+public class Box<T> {
+    private Object object;
+
+    public void set(Object object) {
+        this.object = object;
+    }
+    public Object get() {
+        return object;
+    }
+}
